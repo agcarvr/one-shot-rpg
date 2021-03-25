5d503cf792d8d82f021a5d15b87ae136b00323ee
aaroncarver <agcarvr@gmail.com>
2021-03-24 22:20:43 -0600

    broken infinite loop needs fix

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ const barroChillSprite = document.querySelector('.barroChill');
 const barroMadSprite = document.querySelector('.barroMad');
 const event1Check = document.querySelector('.event1__checkbox');
 
+const attackIcon = document.querySelector('.attackIcon');
+const armorIcon = document.querySelector('.armorIcon');
+
 //BarroLiteral
 const barro = {
     maxHP: 1000,
@@ -19,16 +22,22 @@ class SkellySoldier {
     constructor(level) {
         this.level = level;
         this.maxHP = Math.floor(Math.random() * (this.level * 100)) + (this.level * 50);
-        this.currentHP = 0;
+        this.currentHP = this.maxHP;
         this.evadeChance = Math.random() * 0.3 + 0.1;
         this.Moneypayout = this.level * 10;
         this.xpPayout = this.level * 10;
     }
     attack(enemy) {
         if(Math.random() > enemy.evadeChance){
-            enemy.currentHP -= Math.floor(Math.random() * (this.level * 5));
-            enemy.sprite.style.transform = 'translateX(1rem)';
-            enemy.sprite.style.transform = 'translateX(-1rem)';
+            let attackDamage = Math.floor(Math.random() * (this.level * 5) - enemy.armor);
+            enemy.currentHP -= attackDamage;
+            enemy.armor -= attackDamage;
+            if(enemy.armor < 0){
+                enemy.armor = 0;
+            }
+            console.log('you were attacked by a ' + enemy.name + ' your health is ' + enemy.currentHP);
+        }else {
+            console.log('you dodged him');
         }
     }
 }
@@ -37,6 +46,7 @@ class SkellyBrute extends SkellySoldier {
     constructor(level) {
         super(level)
         this.sprite = skellyBruteSprite;
+        this.name = 'Skeleton Brute';
     }
 }
 
@@ -44,6 +54,7 @@ class SkellyRanger extends SkellySoldier {
     constructor(level) {
         super(level);
         this.sprite = skellyRangerSprite;
+        this.name = 'Skeleton Ranger';
     }
 }
 
@@ -55,22 +66,38 @@ class Crusader {
         this.currentHP = this.maxHP;
         this.evadeChance = .48;
         this.sprite = crusaderSprite;
+        this.name = 'The Crusader';
+        this.armor = 0;
+        this.maxDamage = 20;
     }
     attack(enemy) {
         if(Math.random() > enemy.evadeChance){
-            enemy.currentHP -= Math.floor(Math.random() * (this.level * 20)) + 5;
-            enemy.sprite.style.transform = 'translateX(1rem)';
-            enemy.sprite.style.transform = 'translateX(-1rem)';
+            enemy.currentHP -= Math.floor(Math.random() * this.maxDamage) + 5;
+            console.log('you attacked the ' + enemy.name + ' his health is ' + enemy.currentHP);
+        }else {
+            console.log('he dodged your puny attack nimbly');
         }
     }
 }
 
 const player = new Crusader(1);
 
+const playersAttack = () => {
+    player.attack(currentEnemy);
+    playersAttack.called = true;
+}
+
+const playersArmor = () => {
+    player.armor += Math.floor(Math.random() * 20) + 1;
+    console.log(player.armor);
+    playersArmor.called = true;
+}
+
 //ENEMIES FOR EACH BATTLE
 let currentEnemy = null;
 
 const battle1Brute = new SkellyBrute(5);
+console.log(battle1Brute.currentHP);
 
 const battle2Brute = new SkellyBrute(7);
 
@@ -79,12 +106,23 @@ const battle3Ranger = new SkellyRanger(9);
 const battle4Ranger = new SkellyRanger(7);
 const battle4Brute = new SkellyBrute(7);
 
+//track battle
+let inBattle = false;
+let playerTurn = true;
+
 //ANIMATE AND DEANIMATE CHARACTER SPRITES
 const animateCharacter = (character) => {
-    character.sprite.style.width = '741px';
-    character.sprite.style.height = '741px'; 
-    character.sprite.style.transform = 'scale(.5)'
-    character.sprite.style.opacity = '1';
+    if(character === player){
+        character.sprite.style.width = '160px';
+        character.sprite.style.height = '120px';
+        character.sprite.style.transform = 'scale(2)'
+        character.sprite.style.opacity = '1';
+    } else if(character.sprite === skellyBruteSprite || character.sprite === skellyRangerSprite){
+        character.sprite.style.width = '144px';
+        character.sprite.style.height = '250px';
+        character.sprite.style.transform = 'scale(1.5)'
+        character.sprite.style.opacity = '1';
+    }
 }
 
 const undoAnimateCharacter = (character) => {
@@ -92,23 +130,62 @@ const undoAnimateCharacter = (character) => {
     character.sprite.style.opacity = '0';
 }
 
+const animateButtons = () => {
+    attackIcon.style.width = '512px';
+    attackIcon.style.height = '512px';
+    attackIcon.style.transform = 'scale(.25)';
+    attackIcon.style.opacity = '1'
+    armorIcon.style.width = '512px';
+    armorIcon.style.height = '512px';
+    armorIcon.style.transform = 'scale(.25)';
+    armorIcon.style.opacity = '1';
+}
+
+const undoAnimateButtons = () => {
+    attackIcon.style.height = '0';
+    attackIcon.style.opacity = '0';
+    armorIcon.style.height = '0';
+    armorIcon.style.opacity = '0';
+}
 //Game Loop
-const fightingLoop = (enemy) => {
-    enemy.currentHP = enemy.maxHP
-    while(player.currentHP > 0 && enemy.currentHP > 0){
-        
+const fightingPossibility = (enemy) => {
+    setTimeout(() => { }, 2000);
+    if(playersAttack.called && playerTurn === true){
+        console.log('you attack');
+        playersAttack.called = false;
+        playerTurn = false;
+    }else if(playersArmor === 'called' && playerTurn === true){
+        console.log('you gain armor');
+        playersArmor.called = false;
+        playerTurn = false;
     }
+    if(playerTurn === false){
+        enemy.attack(player);
+        console.log('enemy attacked');
+        playerTurn = true;
+    }    
+}
+
+function fightingLoop() {
+    while (player.currentHP > 0 && currentEnemy.currentHP > 0) {
+        fightingPossibility(currentEnemy);
+    }
+
 }
 
 //Starting from checkbox change
 startBattle = (event) => {
     if(event.target.checked === true){
         animateCharacter(player);
+        eventTarget = event.target;
+        inBattle = true;
         if(event.target === event1Check){
             currentEnemy = battle1Brute;
             animateCharacter(currentEnemy);
+            animateButtons();
         }
-        fightingLoop(currentEnemy);
+        setTimeout(() => { fightingLoop() }, 1000);
+        // fightingLoop(currentEnemy);
     }else {
         crusaderSprite.style.width = '0';
         crusaderSprite.style.opacity = '0';
@@ -118,6 +195,5 @@ startBattle = (event) => {
 
 event1Check.addEventListener('change', startBattle);
 
-if(currentEnemy) {
-    currentEnemy.sprite.addEventListener('click', player.attack);
-}
+attackIcon.addEventListener('click', playersAttack);
+armorIcon.addEventListener('click', playersArmor);
